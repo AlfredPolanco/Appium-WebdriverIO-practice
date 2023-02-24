2d639c5a863c26852a24b9d61e9c807eba5b741d
Alfredo Polanco <alfredpolanco@Alfredos-MacBook-Pro.local>
2023-02-24 13:25:46 -0400

    Adding CI & Prettier

---
+++ tests/specs/android/welcomePage.spec.js
@@ -0,0 +1,10 @@
+const { WelcomePage } = require('../../../pages/welcomePage');
+
+describe('Sample', () => {
+	it('Sample test', async () => {
+		const welcomePage = new WelcomePage();
+		await welcomePage.welcome();
+		await welcomePage.clickOnUnitedKingdom();
+		await welcomePage.tapOnContinueButton();
+	});
+});
